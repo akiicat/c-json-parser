5f16f3d7cd92db4f87cc097c117a3423bd39e390
akiicat <aaaa1379@gmail.com>
2025-01-06 01:41:02 +0000

    fix stdin stream

--- lexer.c
+++ lexer.c
@@ -57,7 +57,7 @@ int nextChar(struct LexerContext *ctx) {
     }
 
     ctx->currentChar = fgetc(ctx->stream);
-    ctx->offset++;
+    ctx->offset = ftell(ctx->stream);
     ctx->row++;
     return ctx->currentChar;
 }
@@ -224,7 +224,7 @@ void getTokenRARRAY(struct LexerContext *ctx) {
 void getTokenCOMMA(struct LexerContext *ctx) {
     unsigned int start = ctx->offset;
     unsigned int startRow = ctx->row;
-    
+
     match(ctx, ',');
 
     insertToken(ctx, (struct Token) {
@@ -759,14 +759,26 @@ int main(int argc, char *argv[]) {
         .offset = 0,
         .column = 1,
         .row = 0,
-        .stream = stdin,
+        .stream = NULL,
     };
 
     if (argc >= 2) {
         lexer_ctx.stream = fopen(argv[1], "r");
+    } else {
+        // redirect stdin to a tempfile, because fseek is not work on stdin.
+        lexer_ctx.stream = tmpfile();
+
+        int c;
+        while ((c = fgetc(stdin)) != EOF)
+            fputc(c, lexer_ctx.stream);
+
+        fflush(lexer_ctx.stream);
+        fseek(lexer_ctx.stream, 0, SEEK_SET);
     }
 
     jsonLexer(&lexer_ctx);
+
+    printToken(&lexer_ctx);
 
     struct ParserContext parser_ctx = {
         .tokenIndex = 0,
@@ -774,11 +786,11 @@ int main(int argc, char *argv[]) {
         .tokenList = lexer_ctx.tokenList,
     };
 
-    printToken(&lexer_ctx);
-
     jsonParser(&parser_ctx);
 
     printTree(&parser_ctx);
+
+    fclose(lexer_ctx.stream);
 
     return 0;
 }
